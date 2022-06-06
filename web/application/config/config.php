68911992fe6371128c5c0325d75ea12b3554cc40
Leanderson <leandersonssouza1@gmail.com>
2022-06-06 09:49:57 -0300

    ci: update base url an project

--- web/application/config/config.php
+++ web/application/config/config.php
@@ -23,13 +23,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://localhost';
 
 /*
 |--------------------------------------------------------------------------
 | Index File
 |--------------------------------------------------------------------------
-|
+|s
 | Typically this will be your index.php file, unless you've renamed it to
 | something else. If you are using mod_rewrite to remove the page set this
 | variable so that it is blank.
