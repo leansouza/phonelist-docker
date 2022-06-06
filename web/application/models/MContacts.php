83a7319ca33c572be77efd18a75ac97d658622a5
Leanderson <leandersonssouza1@gmail.com>
2022-06-06 09:56:02 -0300

    feat: add function getAll

--- web/application/models/MContacts.php
+++ web/application/models/MContacts.php
@@ -3,9 +3,13 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class MContacts extends CI_Model {
-
     
+
+    public function getAll(){
+
+        $query = $this->db->get('customer');
+        return $query->result_array();
+    }
 
 }
 
-/* End of file MContacts.php */
