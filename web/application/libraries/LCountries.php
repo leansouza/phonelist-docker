f6f42cb996dde43b736ff8a8e418d58b2d93bcd1
Leanderson <leandersonssouza1@gmail.com>
2022-06-06 09:40:42 -0300

    add feat extract

--- web/application/libraries/LCountries.php
+++ web/application/libraries/LCountries.php
@@ -13,6 +13,28 @@ class LCountries
         
     }
 
-   
+        /**
+     * Returns a list of countries
+     *
+     * @return array 
+     */
+    public function extract(){
+        $phones = $this->ci->mcontacts->getAll();
+        foreach($phones as $key => $value){
+
+           $returnValidation = $this->ci->lvalidations->hasValidPhoneNumber($this->contries,$value['phone']);
+           
+           if($returnValidation)
+           {
+               $phones[$key]['country'] = $returnValidation[0]; 
+               $phones[$key]['status'] =  $returnValidation[1];
+           }else{
+               return [];
+           }
+        }
+        
+        asort($phones);
+        return $phones;
+    }
 
 }
