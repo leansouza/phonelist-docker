8ad8b5dd0a47f9c93fd75e169e88ae048468f941
Leanderson <leandersonssouza1@gmail.com>
2022-06-06 09:38:43 -0300

    chore: add library lcontries

---
+++ web/application/libraries/LCountries.php
@@ -0,0 +1,18 @@
+<?php
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class LCountries
+{
+    protected $ci;
+
+    public function __construct()
+    {
+        $this->ci =& get_instance();
+        
+    }
+
+   
+
+}
