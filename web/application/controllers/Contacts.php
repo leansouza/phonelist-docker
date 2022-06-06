616b552f17847ec86feadb62ae35700b6f2ff48c
Leanderson <leandersonssouza1@gmail.com>
2022-06-06 09:55:07 -0300

    feat: updated function index to read views

--- web/application/controllers/Contacts.php
+++ web/application/controllers/Contacts.php
@@ -4,17 +4,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Contacts extends CI_Controller {
 
-
     public function __construct()
     {
         parent::__construct();
-        //Do your magic here
+        $this->load->library('LCountries', null, 'lcountries');
     }
-
 
     public function index(){
-        
+        $data['contacts'] = $this->lcountries->extract();
+        $data['countries'] = $this->lcountries->getAll();
+        $this->load->view('includes/header');
+        $this->load->view('contacts/view', $data);
+        $this->load->view('includes/footer');
     }
-
 
 }
