91413e4f3088175fc228101e62d4c257200e64d6
Leanderson <leandersonssouza1@gmail.com>
2022-06-06 09:48:42 -0300

    feat: add function hasValidPhoneNumber

--- web/application/libraries/LValidations.php
+++ web/application/libraries/LValidations.php
@@ -12,6 +12,26 @@ class LValidations
         $this->ci =& get_instance();
     }
 
-   
+        /**
+     * Validates a phone number 
+     *
+     * @param string $phone
+     * @return array
+     */
+    public function hasValidPhoneNumber($countries,$phone): array
+    {
+        preg_match('/\(([0-9].*?)\)/', $phone, $matches);
+        if($matches){
+           $countries = $countries[$matches[1]];
+            if(preg_match($countries[1], $phone, $phonevalid)){
+                return [$countries[0],1];
+            }
+            return [$countries[0],0];
+        }
+        return [];
+        
+    }
+
+    
 
 }
