9b51136d0c39412f19337f5e0c0ccc159839e416
Leanderson <leandersonssouza1@gmail.com>
2022-06-06 09:48:00 -0300

    chore: add library lvalidations

---
+++ web/application/libraries/LValidations.php
@@ -0,0 +1,17 @@
+<?php
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class LValidations
+{
+    protected $ci;
+
+    public function __construct()
+    {
+        $this->ci =& get_instance();
+    }
+
+   
+
+}
